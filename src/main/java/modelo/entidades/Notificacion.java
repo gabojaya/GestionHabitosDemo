8236a2dd4894f2a04dde9923a8536a6a02608a4b
alejandro06020 <alejandro.minda@epn.edu.ec>
2025-01-19 22:33:25 -0500

    notificaciones
    solo le mape a las notificaciones

--- src/main/java/modelo/entidades/Notificacion.java
+++ src/main/java/modelo/entidades/Notificacion.java
@@ -2,16 +2,37 @@ package modelo.entidades;
 
 import java.io.Serializable;
 import java.sql.Date;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name="notificacion")
 public class Notificacion implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="idNotificacion")
 	private int idNotificacion;
+	@Column(name="tipo")
 	private String tipo;
+	@Column(name="mensaje")
 	private String mensaje;
+	@Column(name="fechaEnvio")
 	private Date fechaEnvio;
+	@Column(name="leido")
 	private boolean leido;
+	@OneToOne
+	@JoinColumn(name = "idRecordatorio", nullable = false)
+	private Recordatorio recordatorioAsociado;
+	
 
 	public Notificacion() {
 	}
